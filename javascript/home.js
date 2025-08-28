fd3cab753e01f335f442652765b77e8c31947061
shouravhasanshurjo8201 <shouravhasanshurjo8201@gmail.com>
2025-08-28 12:23:02 +0600

    Copy Buttons Functionality Add

--- javascript/home.js
+++ javascript/home.js
@@ -1,5 +1,6 @@
 
 let heartIconClick = Number(document.getElementById("heart-count").innerText);
+let copyButtonClick = Number(document.getElementById("Copy-count").innerText);
 
 document.getElementById("Emergency-Hotline-Section").addEventListener("click", function(Event){
     // Heart Icons Functionality start
@@ -11,6 +12,19 @@ document.getElementById("Emergency-Hotline-Section").addEventListener("click", f
     }
     // Heart Icons Functionality End
 
-    
+    // Copy Buttons Functionality start
+    if(Event.target.classList.contains("Copy-button")){
+        copyButtonClick = copyButtonClick + 1;
+        document.getElementById("Copy-count").innerText = copyButtonClick;
 
+        const card = Event.target.closest(".Hotline-card");
+        const helplineNumber = card.querySelector(".Helpline-Number").innerText;
+
+        navigator.clipboard.writeText(helplineNumber).then(function(){
+            alert("Copied Number : " + " " + helplineNumber);
+        })
+    }
+    // Copy Buttons Functionality End
+
+   
 })
